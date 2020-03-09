21939a52d23d1a1f72d1996359612b6d6c276e72
Steve Bolton <steviebolton@gmail.com>
2020-03-09 07:22:20 +0100

    initial commit

---
+++ app-swoosh/Views/borderButton.swift
@@ -0,0 +1,21 @@
+//
+//  borderButton.swift
+//  app-swoosh
+//
+//  Created by steve bolton on 09/03/2020.
+//  Copyright © 2020 stevie bolton. All rights reserved.
+//
+
+import UIKit
+
+class borderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
